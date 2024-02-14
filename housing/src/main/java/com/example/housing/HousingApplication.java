4bcdb65c61867ab8f60862f5c312fb03f0c650ca
Daniel Petrov <a12028482@unet.univie.ac.at>
2024-02-14 17:39:36 +0200

    1.0.6 - CRUD on OwnerRepository validated.

--- housing/src/main/java/com/example/housing/HousingApplication.java
+++ housing/src/main/java/com/example/housing/HousingApplication.java
@@ -1,23 +1,13 @@
 package com.example.housing;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class HousingApplication {
 
 	public static void main(String[] args) {
-
-		try {
 			SpringApplication.run(HousingApplication.class, args);
-		}
-		catch(Exception e) {
-			log.error(e.getMessage());
-			//e.printStackTrace();
-		}
-
 	}
 
 }
--- housing/src/test/java/com/example/housing/entity/IOwnerRepositoryTests.java
+++ housing/src/test/java/com/example/housing/entity/IOwnerRepositoryTests.java
@@ -49,4 +49,37 @@ public class IOwnerRepositoryTests {
         assertThat(allOwnersSavedValid, is(true));
     }
 
+    @Test
+    public void testOwnerSuccessfullyDeletedFromDatabase() {
+        final Owner owner = OwnerBuilder.getOwnerA();
+        this.ownerRepo.save(owner);
+
+        this.ownerRepo.deleteById(owner.getOwnerId());
+
+        final Optional<Owner> possibleOwner = this.ownerRepo.findById(owner.getOwnerId());
+
+        assertThat(possibleOwner.isEmpty(), is(true));
+    }
+
+    @Test
+    public void testOwnerSuccessfullyUpdatedInDatabase() {
+        final Owner originalOwner = OwnerBuilder.getOwnerA();
+        this.ownerRepo.save(originalOwner);
+
+        final Owner newOwner = OwnerBuilder.getOwnerB();
+
+        originalOwner.setFirstname(newOwner.getFirstname());
+        originalOwner.setLastname(newOwner.getLastname());
+        originalOwner.setBirthDate(newOwner.getBirthDate());
+
+        this.ownerRepo.save(originalOwner);
+
+        final Optional<Owner> possibleUpdatedOwner = this.ownerRepo.findById(originalOwner.getOwnerId());
+
+        assertThat(possibleUpdatedOwner.isPresent(), is(true));
+        assertThat(possibleUpdatedOwner.get().getFirstname(), equalTo(originalOwner.getFirstname()));
+        assertThat(possibleUpdatedOwner.get().getLastname(), equalTo(originalOwner.getLastname()));
+        assertThat(possibleUpdatedOwner.get().getBirthDate(), equalTo(originalOwner.getBirthDate()));
+    }
+
 }
